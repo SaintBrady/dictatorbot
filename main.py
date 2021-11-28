4dfb2761585204158b04a94531143f9554de850f
SaintBrady <brayden.dyke@gmail.com>
2021-11-27 23:52:15 -0600

    Silenced KeyError on line 298. Fix Me later

--- main.py
+++ main.py
@@ -295,7 +295,7 @@ class Music(commands.Cog):
         ctx.voice_state = self.get_voice_state(ctx)
 
     async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError):
-        await ctx.send('An error occurred: {}'.format(str(error)))
+        #await ctx.send('An error occurred: {}'.format(str(error)))
 
     @commands.command(name='join', invoke_without_subcommand=True)
     async def _join(self, ctx: commands.Context):
